ffa002b0b712c634d55d723bed7b6037f90780e7
Francesco Barbieri <fbarbieri.business@gmail.com>
2023-02-10 21:55:46 +0100

    initial commit

---
+++ Reply2022/main.py
@@ -0,0 +1,15 @@
+# Stamina
+# - Starts with a S_i level of stamina
+# - Consumes during battles
+# - Recovers it afert some time. Max stamina S_max. Exceeding is lost
+# 
+# Total of T turns
+# List of demons D = [] defined by:
+#   - S_c stamina lost by pandora defeating the demon
+#   - T_r number of turns needed to recover stamina after the fight
+#
+#
+#
+#
+#
+#
